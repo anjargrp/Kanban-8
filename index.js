ec9202f45d0eec8779bad4bd96c718b1133a550b
izerhkaf <farhanfakhrezi@gmail.com>
2021-02-26 06:29:33 +0700

    fitur add

--- index.js
+++ index.js
@@ -28,41 +28,65 @@ let list = [
         class: "toDoBox"
     }
 ]
+show(list)
 
-
-let toDoBox = document.getElementById("toDoBox")
-for (var i = 0; i < list.length; i++) {
-    let div = document.createElement("div");
-    div.setAttribute("status", list[i].class)
-    div.setAttribute("draggable", true)
-    div.setAttribute("ondragstart", "drag(event)")
-    div.id = i
-
-    let editButton = document.createElement('button')
-    editButton.innerText = 'Edit'
-    editButton.setAttribute('type', 'submit')
-    editButton.setAttribute('value', i)
-    editButton.classList.add("update-btn")
-    editButton.addEventListener("click", updateBtn)
-
-    let deleteButton = document.createElement('button')
-    deleteButton.innerText = 'delete'
-    deleteButton.setAttribute('type', 'submit')
-    deleteButton.setAttribute('value', i)
-    deleteButton.classList.add("delete-btn")
-    deleteButton.addEventListener("click", deleteBtn)
-
-    let newList = document.createElement("li");
-    newList.innerText = list[i].list
-    newList.id = i
-
-    div.append(newList)
-    div.append(editButton)
-    div.append(deleteButton)
-
-    document.getElementById(list[i].class).appendChild(div)
+function reset(){
+    document.getElementById("toDoBox").innerHTML =""
+    document.getElementById("doingBox").innerHTML =""
+    document.getElementById("doneBox").innerHTML =""
 }
 
+document.getElementById("addToDo").addEventListener("click", add)
+document.getElementById("addDoing").addEventListener("click", add)
+document.getElementById("addDone").addEventListener("click", add)
+
+function add(event){
+    event.preventDefault()
+    let listAdd = prompt("Please Input Your task")
+    if(event.srcElement.id === "addToDo" && listAdd.length > 0){
+        list.push({list: listAdd, class:"toDoBox"})
+    }else if(event.srcElement.id === "addDoing" && listAdd.length > 0){
+        list.push({list: listAdd, class:"doingBox"})
+    }else if(event.srcElement.id === "addDone" && listAdd.length > 0){
+        list.push({list: listAdd, class:"doneBox"})
+    }
+    reset()
+    show(list)
+}
+
+function show(list){
+    for (var i = 0; i < list.length; i++) {
+        let div = document.createElement("div");
+        div.setAttribute("status", list[i].class)
+        div.setAttribute("draggable", true)
+        div.setAttribute("ondragstart", "drag(event)")
+        div.id = i
+
+        let editButton = document.createElement('button')
+        editButton.innerText = 'Edit'
+        editButton.setAttribute('type', 'submit')
+        editButton.setAttribute('value', i)
+        editButton.classList.add("update-btn")
+        editButton.addEventListener("click", updateBtn)
+
+        let deleteButton = document.createElement('button')
+        deleteButton.innerText = 'delete'
+        deleteButton.setAttribute('type', 'submit')
+        deleteButton.setAttribute('value', i)
+        deleteButton.classList.add("delete-btn")
+        deleteButton.addEventListener("click", deleteBtn)
+
+        let newList = document.createElement("li");
+        newList.innerText = list[i].list
+        newList.id = i
+
+        div.append(newList)
+        div.append(editButton)
+        div.append(deleteButton)
+
+        document.getElementById(list[i].class).appendChild(div)
+    }
+}
 function allowDrop(ev) {
     ev.preventDefault();
 }
